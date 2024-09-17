4ffef3e6f2fa4446fe589ce80792dafc3d6699b9
Aldairm0308 <al222111405@gmail.com>
2024-09-17 13:28:53 -0600

    enableCors deleted

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors();
-  app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(3000);
 }
-
 bootstrap();
