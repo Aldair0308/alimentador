b2e27f3ddff5959f4a8701cebdca42c317f23e89
Aldairm0308 <al222111405@gmail.com>
2024-09-20 13:52:04 -0600

    distance module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PetsModule } from './pets/pets.module';
+import { DistanceModule } from './distance/distance.module';
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forRoot('mongodb://mongo:kYNgYYjSUWFsFHEVKQSrSjZkcxNZzkkX@autorack.proxy.rlwy.net:50056'),
     AuthModule,
-    PetsModule
+    PetsModule,
+    DistanceModule
   ],
   controllers: [],
   providers: [],
