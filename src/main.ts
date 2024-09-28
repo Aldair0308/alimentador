23a32d30382178c6db068c1570bac9093f8a4f71
Aldairm0308 <al222111405@gmail.com>
2024-09-28 10:11:48 -0600

    prefix added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  // Configurar el prefijo de la api
+  // app.setGlobalPrefix('api/v1');
+
   await app.listen(3000);
 }
 bootstrap();
